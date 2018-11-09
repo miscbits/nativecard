d4017509eda2b55ef6f1a1e6771c3b10a8fdd760
miscbits <willow6786@gmail.com>
2018-11-08 19:01:13 -0500

    added shufflee and worked on building project for ios and android

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,7 +1,8 @@
 <template>
     <Page>
-        <ActionBar title="FlashCard Vue">
+        <ActionBar title="ZC FlashCards">
           <ActionItem text="Next Card" android.systemIcon="Next Card" @tap="nextCard()" />
+          <ActionItem text="Shuffle" android.systemIcon="Shuffle" @tap="shuffleDeck()" />
         </ActionBar>
         <FlashCard row=1 col=0 v-bind:flashcard="flashcard" v-bind:flipped="flashcard.flipped"/>
     </Page>
@@ -25,13 +26,31 @@
             }
         },
         methods: {
+            shuffle: function (array) {
+              var currentIndex = array.length, temporaryValue, randomIndex;
+
+              // While there remain elements to shuffle...
+              while (0 !== currentIndex) {
+
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+              }
+
+              return array;
+            },
             getCards: function() {
                 http.request({
                   url: "https://fc.wilhemalcivar.com/api/flashcards",
                   method: "GET",
                   headers: { "Content-Type": "application/json" }
                 }).then(response => {
-                    this.flashcards = response.content.toJSON();
+                    this.flashcards = this.shuffle(response.content.toJSON());
                     this.flashcard = this.flashcards[0];
                     this.current_card = 0;
                 }, error => {
@@ -44,10 +63,21 @@
                     var card = this.flashcards[this.current_card];
                     card.flipped = false;
                     this.flashcard = card;
+                } else {
+                    this.flashcard = {
+                        front: "Last card. Click shuffle to start over",
+                        back: "Last card. Click shuffle to start over",
+                        flipped: false
+                    }
                 }
             },
             onLoaded: function() {
                 this.getCards();
+            }, 
+            shuffleDeck: function() {
+                this.flashcards = this.shuffle(this.flashcards);
+                this.flashcard = this.flashcards[0];
+                this.current_card = 0;
             }
         },
         components: {
--- app/components/FlashCard.vue
+++ app/components/FlashCard.vue
@@ -1,8 +1,8 @@
 <template>
     <GridLayout columns="*, 5*, *" rows="*, 3*, *" backgroundColor="#3c495e">
         <Button col=1 row=1 @tap="flip" backgroundColor="#43b883"/>
-        <Button col=1 row=1 class=message v-if="!this.flipped" @tap="flip" v-bind:text="flashcard.front" backgroundColor="#43b883"/>
-        <Button col=1 row=1 class=message v-if="this.flipped" @tap="flip" v-bind:text=flashcard.back backgroundColor="#43b883"/>
+        <Button col=1 row=1 class=message v-if="!this.flipped" @tap="flip" v-bind:text="flashcard.front" backgroundColor="#43b883" textWrap="true" />
+        <Button col=1 row=1 class=message v-if="this.flipped" @tap="flip" v-bind:text=flashcard.back backgroundColor="#43b883" textWrap="true" />
     </GridLayout>
 </template>
 
